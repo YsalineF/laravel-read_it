eddd873f4dd267150c49132a48092580f3d390d8
Mézz <ysaline.franquet@gmail.com>
2021-01-25 13:57:14 +0100

    Route posts.show - Comments & Add Form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -26,51 +26,10 @@
 
 
    <div class="pt-5 mt-5">
-     <h3 class="mb-5">3 Comments</h3>
-     <ul class="comment-list">
-       <li class="comment">
-         <div class="comment-body">
-           <h3>John Doe</h3>
-           <div class="meta mb-3">November 13, 2019 at 2:21pm</div>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-         </div>
-       </li>
+     {{-- Commentaires du post --}}
+        @include('comments._post_comments', ['comments' => $post->comments])
 
-       <li class="comment">
-         <div class="comment-body">
-           <h3>John Doe</h3>
-           <div class="meta mb-3">November 13, 2019 at 2:21pm</div>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-         </div>
-       </li>
-
-       <li class="comment">
-         <div class="comment-body">
-           <h3>John Doe</h3>
-           <div class="meta mb-3">November 13, 2019 at 2:21pm</div>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-         </div>
-       </li>
-     </ul>
-     <!-- END comment-list -->
-
-     <div class="comment-form-wrap pt-5">
-       <h3 class="mb-5">Leave a comment</h3>
-       <form action="#" class="p-5 bg-light" method="post">
-         <div class="form-group">
-           <label for="name">Name *</label>
-           <input type="text" class="form-control" id="name">
-         </div>
-         <div class="form-group">
-           <label for="message">Message</label>
-           <textarea name="" id="message" cols="30" rows="10" class="form-control"></textarea>
-         </div>
-         <div class="form-group">
-           <input type="hidden" name="postId" value="4" />
-           <input type="submit" value="Post Comment" class="btn py-3 px-4 btn-primary">
-         </div>
-
-       </form>
-     </div>
+     {{-- Formulaire d'ajout d'un commentaire --}}
+       @include('comments._post_form_add')
    </div>
  @endsection
