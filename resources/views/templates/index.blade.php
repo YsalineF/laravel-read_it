22e82f14e7bc78170565e9e40e1d50c67374b778
Mézz <ysaline.franquet@gmail.com>
2021-01-25 13:16:51 +0100

    Intégration et découpe du template

---
+++ resources/views/templates/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('templates.partials._head')
+  </head>
+  <body>
+
+	  @include('templates.partials._nav')
+
+    @include('templates.partials._hero')
+
+    @include('templates.partials._main')
+
+    @include('templates.partials._footer')
+
+  @include('templates.partials._loader')
+
+  @include('templates.partials._scripts')
+
+  </body>
+</html>
---
+++ resources/views/templates/partials/_main.blade.php
@@ -0,0 +1,13 @@
+<section class="ftco-section ftco-degree-bg">
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-8 ftco-animate">
+         @yield('content')
+       </div>
+
+       <!-- .col-md-8 -->
+       @include('templates.partials._aside')
+
+     </div>
+   </div>
+ </section> <!-- .section -->
