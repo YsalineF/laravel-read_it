48f2e28e8cba3ffec850cbc164c81b22ddea026e
Mézz <ysaline.franquet@gmail.com>
2021-01-25 13:49:08 +0100

    Route posts.show - Title & content

--- app/Http/Controllers/Posts.php
+++ app/Http/Controllers/Posts.php
@@ -7,10 +7,25 @@ use App\Models\Post;
 
 class Posts extends Controller
 {
+
+    /**
+     * [index description]
+     * @param  integer $limit [description]
+     * @return [type]         [description]
+     */
     public function index(INT $limit = 10) {
       $posts = Post::orderBy('created_at', 'desc')
                     ->take($limit)
                     ->get();
       return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * [show description]
+     * @param  Post     $post [description]
+     * @return [type]         [description]
+     */
+    public function show(Post $post) {
+      return view('posts.show', compact('post'));
     }
 }
